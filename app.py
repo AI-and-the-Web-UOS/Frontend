5fce78e4db360f2afd21c957309ca7cf54d3a92e
Jonah Schlie <82595001+jonahschlie@users.noreply.github.com>
2023-11-14 00:02:06 +0100

    api logic and result UI

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from sent2vec.vectorizer import Vectorizer
+import requests
 
-vectorizer = Vectorizer()
 app = Flask(__name__)
 
 
 def search_embedding(search_term):
+    vectorizer = Vectorizer()
     vectorizer.run([search_term])
-    return vectorizer.vectors
+    return vectorizer.vectors[0].tolist()
 
 
 @app.route('/')
@@ -17,16 +18,34 @@ def default():
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        search_term = request.form.get('search_term')
-        vector = search_embedding(search_term)
-        return redirect(url_for('results', search_term=search_term))
     return render_template('search.html')
 
 
 @app.route('/results/<search_term>')
 def results(search_term):
-    return render_template('results.html', search_term=search_term)
+    vector = search_embedding(search_term)
+    data = {
+        "Vector": vector
+    }
+
+    url = "http://localhost:5001/search"
+    response = requests.get(url, json=data)
+    result = response.json()
+    data = result["results"]
+    if response.status_code == 200:
+        return render_template('results.html', search_text=search_term, data=data)
+    else:
+        return render_template('search.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    search_text = request.form['search_text']
+
+    if not search_text:
+        return redirect(url_for('index', error='Bitte gib einen Suchtext ein!'))
+
+    return redirect(url_for('results', search_term=search_text))
 
 
 if __name__ == '__main__':
